4301753765ae1548e668c109190f3d7a23c55ad5
Soumyadeep Saha <soumyadeep1303@gmail.com>
2023-08-16 14:33:34 +0530

    move_forward

--- craft_control.py
+++ craft_control.py
@@ -1,6 +1,16 @@
 def move_forward(position, direction):
    if direction == "N":
       position[1] += 1
+   elif direction == "S":
+      position[1] -= 1
+   elif direction == "E":
+      position[0] += 1
+   elif direction == "W":
+      position[0] -= 1
+   elif direction == "U":
+      position[2] += 1
+   elif direction == "D":
+      position[2] -= 1
 
 def move_backward(position, direction):
       if direction == "U":
@@ -10,6 +20,7 @@ def turn_left(direction):
    if direction == 'U':
       return 'N'
 
+
 def turn_right(direction):
    if direction == 'N':
       return 'E'
@@ -18,20 +29,22 @@ def turn_up(direction):
    if direction == 'E':
       return 'U'
 
-def turn_down(position, direction):
+def turn_down(direction):
    return
 
 def chandrayaan_3(position, direction, commands):
    for command in commands:
       if command == "f":
          move_forward(position, direction)
-      if command == "b":
+      elif command == "b":
          move_backward(position, direction)
-      if command == "r":
+      elif command == "r":
          direction = turn_right(direction)
-      if command == "l":
+      elif command == "l":
          direction = turn_left(direction)
-      if command == "u":
+      elif command == "u":
          direction = turn_up(direction)
+      elif command == "d":
+         direction = turn_down(direction)
 
    return position, direction
